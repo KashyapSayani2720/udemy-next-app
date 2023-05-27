044d2bb5f1f9601bd5b5ae9b97746a3662e812cc
Kashyap Sayani <kashyapsayani1@gmail.com>
2023-05-27 15:25:11 +0530

    Added Events

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,6 +1,32 @@
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy_data";
+import { Fragment } from "react";
+
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+
+
 function EventDetailsPage() {
+
+    const router = useRouter();
+
+    const eventId = router.query.eventId;
+
+    const event = getEventById(eventId);
+
+    if(!event){
+        return <p>No Event Found !!!</p>
+    }
+
     return (
-        <h1>Event Details Page</h1>
+        <Fragment>
+            <EventSummary title={event.title} />
+            <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title} />
+            <EventContent>
+                <p>{event.description}</p>
+            </EventContent>
+        </Fragment>
     );
 }
 
